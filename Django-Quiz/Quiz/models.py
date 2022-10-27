805225a2f0b7ea5eb46f7d7b77c37c1032c4152f
BrandonVS <vegasotobrandon@yahoo.com>
2022-10-27 10:41:51 -0500

    Test Simpful

--- Django-Quiz/Quiz/models.py
+++ Django-Quiz/Quiz/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.conf import settings
 
 from Quiz.pruebasimpful import sistemaFuzzy
-from .pruebasimpful import obtener_dif_pregunta
+from .sistemafuzzy import obtener_dif_pregunta
 
 import random
 
--- Django-Quiz/Quiz/pruebasimpful.py
+++ Django-Quiz/Quiz/pruebasimpful.py
@@ -1,77 +1,50 @@
 from simpful import *
 
-difSig = 0
+calificacion = 0
+tiempo = 0
+ayuda = 0
 
-def sistemaFuzzy(calificacion, tiempo, ayuda, dificultad):
-	global difSig
-	if ayuda == False:
-		ayuda = 0
-	else:
-		ayuda = 1
-	
-	FS = FuzzySystem()
 
-	# Define fuzzy sets and linguistic variables
-	# Calificacion
-	C_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=1), term="incorrecto")
-	C_2 = FuzzySet(function=Triangular_MF(a=0, b=1, c=1), term="correcto")
-	FS.add_linguistic_variable("calificacion", LinguisticVariable([C_1, C_2], concept="Calificacion", universe_of_discourse=[0,1]))
+FS = FuzzySystem()
 
-	# Tiempo
-	T_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=5, d=6), term="poco")
-	T_2 = FuzzySet(function=Trapezoidal_MF(a=6, b=6.5, c=7, d=7.5), term="medio")
-	T_3 = FuzzySet(function=Trapezoidal_MF(a=7.5, b=8, c=9, d=9), term="mucho")
-	FS.add_linguistic_variable("tiempo", LinguisticVariable([T_1, T_2, T_3], concept="Tiempo por pregunta", universe_of_discourse=[0,90]))
+# Define fuzzy sets and linguistic variables
+# Calificacion
+C_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=1), term="incorrecto")
+C_2 = FuzzySet(function=Triangular_MF(a=0, b=1, c=1), term="correcto")
+FS.add_linguistic_variable("calificacion", LinguisticVariable([C_1, C_2], concept="Calificacion", universe_of_discourse=[0,1]))
 
-	# Ayuda
-	A_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=1), term="no")
-	A_2 = FuzzySet(function=Triangular_MF(a=0, b=1, c=1), term="si")
-	FS.add_linguistic_variable("ayudas", LinguisticVariable([A_1, A_2], concept="Utilizo ayuda", universe_of_discourse=[0,1]))
+# Tiempo
+T_1 = FuzzySet(function=Trapezoidal_MF(a=0, b=0, c=50, d=60), term="poco")
+T_2 = FuzzySet(function=Trapezoidal_MF(a=60, b=65, c=70, d=75), term="medio")
+T_3 = FuzzySet(function=Trapezoidal_MF(a=75, b=80, c=90, d=90), term="mucho")
+FS.add_linguistic_variable("tiempo", LinguisticVariable([T_1, T_2, T_3], concept="Tiempo por pregunta", universe_of_discourse=[0,90]))
 
-	# Define output fuzzy sets and linguistic variable
-	# Dificultad sube o baja
-	D_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.5), term="baja")
-	D_2 = FuzzySet(function=Triangular_MF(a=0.5, b=1, c=1), term="suba")
-	FS.add_linguistic_variable("dificultadSig", LinguisticVariable([D_1, D_2], universe_of_discourse=[0,1]))
+# Ayuda
+A_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=1), term="no")
+A_2 = FuzzySet(function=Triangular_MF(a=0, b=1, c=1), term="si")
+FS.add_linguistic_variable("ayudas", LinguisticVariable([A_1, A_2], concept="Utilizo ayuda", universe_of_discourse=[0,1]))
 
-	# Define fuzzy rules
-	R1 = "IF (calificacion IS incorrecto) OR (tiempo IS mucho) OR (ayudas IS si) THEN (dificultadSig IS baja)"
-	R2 = "IF (calificacion IS correcto) AND (tiempo IS medio) AND (ayudas IS no) THEN (dificultadSig IS sube)"
-	R3 = "IF (calificacion IS correcto) AND (tiempo IS poco) AND (ayudas IS no) THEN (dificultadSig IS sube)"
-	FS.add_rules([R1, R2, R3])
+# Define output fuzzy sets and linguistic variable
+# Dificultad sube o baja
+D_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=0.5), term="baja")
+D_2 = FuzzySet(function=Triangular_MF(a=0.5, b=1, c=1), term="suba")
+FS.add_linguistic_variable("dificultadSig", LinguisticVariable([D_1, D_2], universe_of_discourse=[0,1]))
 
-	# Set antecedents values
-	FS.set_variable("calificacion", calificacion) 
-	FS.set_variable("tiempo", tiempo)
-	FS.set_variable("ayudas", ayuda)
+# Define fuzzy rules
+R1 = "IF (calificacion IS incorrecto) OR (tiempo IS mucho) OR (ayudas IS si) THEN (dificultadSig IS baja)"
+R2 = "IF (calificacion IS correcto) AND (tiempo IS medio) AND (ayudas IS no) THEN (dificultadSig IS sube)"
+R3 = "IF (calificacion IS correcto) AND (tiempo IS poco) AND (ayudas IS no) THEN (dificultadSig IS sube)"
+FS.add_rules([R1, R2, R3])
 
-	dificultadSig = FS.inference(['dificultadSig'])
+# Set antecedents values
+FS.set_variable("calificacion", calificacion) 
+FS.set_variable("tiempo", tiempo)
+FS.set_variable("ayudas", ayuda)
 
-	difSig = dificultadSig.get('dificultadSig')
+dificultadSig = FS.inference(['dificultadSig'])
 
-	print('Las variables de entrada son:\nCalificacion: ' + str(calificacion) + '\nTiempo: ' + str(tiempo) + '\nAyudas: ' + str(ayuda))
+difSig = dificultadSig.get('dificultadSig')
 
-	print('Resultado simpful:' + str(difSig))
+print('Las variables de entrada son:\nCalificacion: ' + str(calificacion) + '\nTiempo: ' + str(tiempo) + '\nAyudas: ' + str(ayuda))
 
-	definir_dificultad(difSig, dificultad)
-	
-	print('Dificultad anterior: ' + str(dificultad))
-	print('Dificultad siguiente: ' + str(difSig))
-
-def definir_dificultad(dif, dificultad):
-	global difSig
-	if dif <= 0.5:
-		difSig = dificultad-1
-
-	if difSig < 1:
-		difSig = 1
-
-	if dif > 0.5:
-		difSig = dificultad+1
-
-	if difSig > 3:
-		difSig = 3
-
-def obtener_dif_pregunta():
-	global difSig
-	return difSig
+print('Resultado simpful:' + str(difSig))
